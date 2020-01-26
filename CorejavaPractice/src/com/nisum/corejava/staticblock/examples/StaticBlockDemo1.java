81f9240e4661a45a50d8b7d9e0b03ebca323fa87
Syed Riyaz Uddin <sriyaz@nisum.com>
2020-01-27 01:03:05 +0530

    CoreJava practice

---
+++ CorejavaPractice/src/com/nisum/corejava/staticblock/examples/StaticBlockDemo1.java
@@ -0,0 +1,16 @@
+//program to create a static block
+package com.nisum.corejava.staticblock.examples;
+
+public class StaticBlockDemo1 {
+
+    // This is a static method (method with a keyword "static"
+    static void myMethod() {
+        System.out.println("Static method called");
+    }
+
+    public static void main(String[] args) {
+        //we are call this method without creating any object.
+
+        myMethod();
+    }
+}
