ba184af4cabaa375b1d71711ed6e54c62f3168d9
Syed Riyaz Uddin <sriyaz@nisum.com>
2020-01-26 23:35:52 +0530

    CoreJava practice

---
+++ CorejavaPractice/src/com/nisum/corejava/basics2/examples/SqaureAreaDemo.java
@@ -0,0 +1,23 @@
+//program to calculate the area of a square
+
+package com.nisum.corejava.basics2.examples;
+
+import java.util.Scanner;
+
+class SquareAreaDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter Side of Square:");
+
+        //taking the user's input
+        Scanner scanner = new Scanner(System.in);
+
+        //Storing the captured value in a variable
+        double side = scanner.nextDouble();
+
+        //Area of Square = side*side
+        double area = side * side;
+        System.out.println("Area of Square is: " + area);
+    }
+}
