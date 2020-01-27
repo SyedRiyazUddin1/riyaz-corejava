69b9707ffc5842f2a1ebe01aceb3e1245ac3c5e1
Syed Riyaz Uddin <sriyaz@nisum.com>
2020-01-27 11:10:53 +0530

    CoreJava practice

---
+++ CorejavaPractice/src/com/nisum/corejava/filehandling/bytestreams/FileInputStreamDemo1.java
@@ -0,0 +1,19 @@
+package com.nisum.corejava.filehandling.bytestreams;
+
+import java.io.FileInputStream;
+
+public class FileInputStreamDemo1 {
+	public static void main(String[] args) {
+		try {
+			FileInputStream fin = new FileInputStream("D:\\Java Programs\\TestFile.txt");
+			int i = fin.read(); // this method will print only 1 character from the file.
+			System.out.print((char) i);
+
+			fin.close();
+		} catch (Exception e) {
+			System.out.println(e);
+
+		}
+
+	}
+}
---
+++ CorejavaPractice/src/com/nisum/corejava/filehandling/serialization_deserialization/SerializeDemo.java
@@ -0,0 +1,25 @@
+package com.nisum.corejava.filehandling.serialization_deserialization;
+
+import java.io.*;
+public class SerializeDemo {
+
+   public static void main(String [] args) {
+      EmployeeDemo e = new EmployeeDemo ();
+      e.name = "Syed Riyaz Uddin";
+      e.address = "Hyderabad, Telangana";
+      e.aadhar = 38555822;
+      e.number = 101;
+      
+      try {
+         FileOutputStream fileOut = new FileOutputStream("D:\\Java Programs\\Serialize Demo.txt");
+         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+         out.writeObject(e);
+         out.close();
+         
+         fileOut.close();
+         System.out.printf("Serialized data is saved in D:\\Java Programs\\Serialize Demo.txt");
+      } catch (IOException i) {
+         i.printStackTrace();
+      }
+   }
+}
