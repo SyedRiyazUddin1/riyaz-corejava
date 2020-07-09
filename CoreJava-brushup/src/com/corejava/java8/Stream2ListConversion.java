8f3094f7bee869b0ba5465d8b0eeeb592c33309f
SyedRiyazUddin1 <59564957+SyedRiyazUddin1@users.noreply.github.com>
2020-07-10 00:16:59 +0530

    Add files via upload

--- CoreJava-brushup/src/com/corejava/java8/Stream2ListConversion.java
+++ CoreJava-brushup/src/com/corejava/java8/Stream2ListConversion.java
@@ -12,17 +12,17 @@ public class Stream2ListConversion {
 	public static void main(String[] args) {
 		// creating List
 		List<Integer> EmpList = new ArrayList<Integer>();
-		for (int i = 1; i <= 12; i++) {
-			EmpList.add(i);
+		for (int loop = 1; loop <= 12; loop++) {
+			EmpList.add(loop);
 		}
 		Stream<Integer> stream = EmpList.stream(); // created stream of List
 
-//	         stream.forEach(i -> System.out.println(i)); can't reuse the stream, will get IllegalStateException here.
+//	         stream.forEach(num -> System.out.println(num)); can't reuse the stream, will get IllegalStateException here.
 
 		// converting Stream to List again and , multiplying it and collecting
 		List<Integer> evenNumbersList = stream.filter(numbers -> numbers % 2 == 0) // filtering only even numbers
 																					// list
-				.map(s -> s * s)// multiplying the list by 2
+				.map(numbers -> numbers * numbers)// multiplying the list by 2
 				.distinct().skip(3) // skips the starting three objects
 				.collect(Collectors.toList()); // Collecting the stream and converting it in list
 		System.out.println(evenNumbersList);
